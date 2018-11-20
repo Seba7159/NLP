e77524847291cbed2d72a1416526722d98215d95
root <root@localhost.localdomain>
2018-11-20 20:56:36 +0000

    Files are now read correctly

--- main.py
+++ main.py
@@ -10,14 +10,34 @@ from os.path import isfile, join
 
 myPath = "untagged/"
 onlyfiles = [f for f in listdir(myPath) if isfile(join(myPath, f))]
-print(onlyfiles)
+
+
+# Declare hash map of files
+mapFiles   = {}
+mapHeaders = {}
+mapContent = {}
 
 
 # Reading the corpora
-import nltk
-corpus_root = 'untagged/'
-corpus = nltk.corpus.reader.plaintext.PlaintextCorpusReader(corpus_root, onlyfiles)
-print(corpus)
+for fileName in onlyfiles:
+    # Construct file name and read the file
+    filePath = myPath + fileName
+    file = open(filePath, "r")
+    fileContent = file.read()
+
+    # Map file content by file name
+    mapFiles[fileName] = fileContent
+
+    # Map headers and contents to specific hash maps
+    splitter = "Abstract:"
+    splitFileContent = fileContent.split(splitter)
+    mapHeaders[fileName] = splitFileContent[0]
+    if splitter in fileContent:
+        mapContent[fileName] = splitFileContent[1]
+    else:
+        mapContent[fileName] = ""
 
 
-#
+# Testing if a file has been added correctly
+print("Header: \n" + mapHeaders["301.txt"])
+print("Content: \n" + mapContent["301.txt"])
