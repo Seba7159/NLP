75de7df8835b2633c087f5c673e68b0a464fb2f9
root <root@localhost.localdomain>
2018-11-22 19:27:53 +0000

    Created a better structure for the project

--- main.py
+++ main.py
@@ -55,15 +55,13 @@ def tag(position, word, fileName, tagName):
 
 
 # Tag times using regex's
-def tagTimes():
-    testFileName = "303.txt"
-
+def tagTimes(fileName):
     # Initialise key for hash map tags
-    mapTags[testFileName] = {}
+    mapTags[fileName] = {}
 
     # Tag start and end time from headers
     headerRegEx = "Time:(.*)"
-    headerTimesTemp = re.search(headerRegEx, mapHeaders[testFileName])
+    headerTimesTemp = re.search(headerRegEx, mapHeaders[fileName])
 
     # If header times are not found
     if headerTimesTemp is None:
@@ -72,14 +70,14 @@ def tagTimes():
     headerTimes = headerTimesTemp.group(1).split("-")
 
     if len(headerTimes) == 1:
-        mapTags[testFileName]['stime'] = headerTimes[0].strip()
-        mapTags[testFileName]['etime'] = "PARAMETER_EMPTY"
+        mapTags[fileName]['stime'] = headerTimes[0].strip()
+        mapTags[fileName]['etime'] = "PARAMETER_EMPTY"
     elif len(headerTimes) == 2:
-        mapTags[testFileName]['stime'] = headerTimes[0].strip()
-        mapTags[testFileName]['etime'] = headerTimes[1].strip()
+        mapTags[fileName]['stime'] = headerTimes[0].strip()
+        mapTags[fileName]['etime'] = headerTimes[1].strip()
     else:
-        mapTags[testFileName]['stime'] = "PARAMETER_EMPTY"
-        mapTags[testFileName]['etime'] = "PARAMETER_EMPTY"
+        mapTags[fileName]['stime'] = "PARAMETER_EMPTY"
+        mapTags[fileName]['etime'] = "PARAMETER_EMPTY"
 
     # Find times in content
     timeRegEx = re.compile("\\b((1[0-2]|0?[1-9])((:[0-5][0-9])?)(\s?)([AaPp][Mm])|(1[0-2]|0?[1-9])(:[0-5][0-9])){1}")
@@ -89,26 +87,60 @@ def tagTimes():
     TIME_TAG_LEN = 15
 
     # Add tags at start and end of time
-    for m in timeRegEx.finditer(mapFiles[testFileName]):
+    for m in timeRegEx.finditer(mapFiles[fileName]):
         position = m.start() + counter * TIME_TAG_LEN
         wordToTag = m.group().strip()
-        if wordToTag.lower()   == mapTags[testFileName]['stime'].lower():
-            tag(position, wordToTag, testFileName, 'stime')
+        if wordToTag.lower()   == mapTags[fileName]['stime'].lower():
+            tag(position, wordToTag, fileName, 'stime')
             counter += 1
-        elif wordToTag.lower() == mapTags[testFileName]['etime'].lower():
-            tag(position, wordToTag, testFileName, 'etime')
+        elif wordToTag.lower() == mapTags[fileName]['etime'].lower():
+            tag(position, wordToTag, fileName, 'etime')
             counter += 1
 
     # End method for time tagging
     return
 
 
-# Method for tagging sentences 
-def tagSentences():
+# Method for tagging sentences
+def tagSentences(fileName):
+    return
+
+
+# Method for tagging paragraphs
+def tagParagraphs(fileName):
+    return
+
+
+# Method for tagging the speaker of the annoucement
+def tagSpeaker(fileName):
+    return
+
+
+# Method to tag the topic
+def tagTopic(fileName):
+    return
+
+
+# Method for tagging the location
+def tagLocation(fileName):
     return
 
 
 # Main code
 if __name__ == '__main__':
+    # Read the file contents from the 'untagged' folder
     readContents()
-    tagTimes()
+
+    # Set the file name
+    fileName = "303.txt"
+
+    # Tag in order
+    tagParagraphs(fileName)
+    tagSentences(fileName)
+    tagSpeaker(fileName)
+    tagTopic(fileName)
+    tagLocation(fileName)
+    tagTimes(fileName)
+
+    # Print content
+    print(mapFiles[fileName])
